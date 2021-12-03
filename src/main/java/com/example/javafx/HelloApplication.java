94aea90bcbe122c201fa91681cc9cd2c302786eb
Tadhg McCarthy <tadhg.mcc77@gmail.com>
2021-12-03 19:57:46 +0000

    import junit

--- src/main/java/com/example/javafx/HelloApplication.java
+++ src/main/java/com/example/javafx/HelloApplication.java
@@ -4,8 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import org.junit.*;
+
 
 public class HelloApplication extends Application {
     @Override
@@ -20,4 +21,5 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.javafx {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires junit;
 
     opens com.example.javafx to javafx.fxml;
     exports com.example.javafx;
