27331ed3d08cb8ccc9122b1e0da3f3fbe42eee52
Tadhg McCarthy <tadhg.mcc77@gmail.com>
2021-12-03 20:57:18 +0000

    Create Testclass for registration javaFX class

---
+++ src/main/java/com/example/javafx/Registration.java
@@ -0,0 +1,33 @@
+package com.example.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.io.IOException;
+import org.junit.*;
+
+
+public class Registration extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
+
+
+
